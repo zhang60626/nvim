9d1edc5a1cc083b851222b3da6f7a594e3b07cf9
zhang60626 <zhangjian60626@gmail.com>
2022-11-05 00:05:15 +0800

    lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -100,6 +100,9 @@ return packer.startup(function(use)
     use "akinsho/bufferline.nvim"
     use "moll/vim-bbye"
 
+    -- lualine
+    use 'nvim-lualine/lualine.nvim'
+
     use "aklt/plantuml-syntax" -- plantuml highlight plugin
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
