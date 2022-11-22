4e709398ac7fa0a9b0bb3724622452e2b7a0782e
zhang60626 <zhangjian60626@gmail.com>
2022-11-22 10:45:24 +0800

    update colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,6 @@
 --local colorscheme = "default"
-local colorscheme = "darkplus"
+local colorscheme = "nordfox"
+
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
